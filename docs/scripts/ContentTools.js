33f4e35c4df7e893220ad0405cd1fe143b8f4175
CalebStride <22614395+CalebStride@users.noreply.github.com>
2023-10-23 22:00:11 +0100

    Fix some css and allow site to work without js

--- docs/scripts/ContentTools.js
+++ docs/scripts/ContentTools.js
@@ -6,13 +6,13 @@ class ContentTools {
         if (element.classList.contains("selectedDD")) {
             this.hideOrShow(id);
             element.classList.toggle("selectedDD");
-            window.sessionStorage.setItem(element.id.replace("DropDownB", ""), false);
+            window.sessionStorage.setItem(element.id.replace("DropDownB", ""), 'false');
         } else {
             if (element.nextElementSibling.classList.contains("hiddenFeature")) {
                 this.hideOrShow(id);
             } 
             element.classList.toggle("selectedDD");
-            window.sessionStorage.setItem(element.id.replace("DropDownB", ""), true);
+            window.sessionStorage.setItem(element.id.replace("DropDownB", ""), 'true');
         }
 
     }
@@ -46,9 +46,12 @@ class ContentTools {
 
     // Sets the nav bar button as selected
     static setElementSelectedNav(element, pageName) {
-        if (window.sessionStorage.getItem(pageName) == 'true') {
+        if (window.sessionStorage.getItem(pageName) === 'true') {
             element.classList.add('selectedDD');
-            element.nextElementSibling.classList.remove('hiddenFeature')
+            element.nextElementSibling.classList.remove('hiddenFeature');
+        } else {
+            element.classList.remove('selectedDD');
+            element.nextElementSibling.classList.add('hiddenFeature');
         }
     }
 }
